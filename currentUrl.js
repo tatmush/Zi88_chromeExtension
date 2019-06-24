599e5ffe5c2436d6c7e483d44e927ac01700f789
tatmush <tatalmondmush@gmail.com>
2019-06-24 16:55:46 +0200

    successfully posting to twitter

--- currentUrl.js
+++ currentUrl.js
@@ -3,21 +3,24 @@ document.addEventListener("DOMContentLoaded", () => {
 	chrome.tabs.query({currentWindow: true, active: true}, (tabs) => {
 		chrome.tabs.sendMessage(tabs[0].id, "", getStats)
 		});
-
+	
+	//clear all data button
 	document.getElementById("clearAllData").addEventListener("click", clearAllData);
 
+	//tweet button
+	document.getElementById("tweet").addEventListener("click", tweet);
 	}, false);
 
 function getStats(stats){
-
+	console.log(stats);
 	musicList = document.getElementById("playList");
 	Object.entries(stats).forEach(
 		([key, value]) => {
-
+			
 			index = key.indexOf("v=");
 			videoId = key.slice(index+2);
 			videoData = getVideoInfo(videoId, value);
-	});
+		});
 }
 
 function getVideoInfo(videoId, value){
@@ -35,6 +38,9 @@ function getVideoInfo(videoId, value){
 		videoCategory = data.items[0].snippet.categoryId;
 		//Music video categoryId is "10" for youtube
 		if (videoCategory === "10"){
+			
+			videoTitle = data.items[0].snippet.title;
+
 			//make an li tag for each video
 			let li = document.createElement("li");
 			li.className = "d-flex flex-row";
@@ -47,7 +53,7 @@ function getVideoInfo(videoId, value){
 			let videoUrl = document.createElement("a");
 			videoUrl.href = "https://www.youtube.com/watch?v=" + videoId;
 			videoUrl.target = "_blank";
-			videoUrl.textContent = data.items[0].snippet.title;
+			videoUrl.textContent = videoTitle;
 				
 			let timesWatched = document.createElement("span");
 
@@ -109,3 +115,35 @@ function clearAllData(){
 
 	document.getElementById("playList").innerHTML = "";
 }
+
+function tweet(){
+	let text = "";
+
+	ulTag = document.getElementById("playList");
+	liTags = document.getElementsByTagName("li");
+	
+	liTagArray=Object.values(liTags);
+	liTagArray.forEach(liTag => {
+    		divs = liTag.getElementsByTagName("div");
+		//get the title of the video
+		titleDiv = divs[1];
+	    	link = titleDiv.getElementsByTagName("a")[0];
+		videoTitle = link.innerHTML;
+		videoLink = link.href
+		
+		countDiv = divs[2];
+		span = countDiv.getElementsByTagName("span")[0];
+		count = span.innerText
+
+		text = text + videoTitle + "\t" + "-" + "\t" + count + "\n";
+		//text.concat(videoTitle, "_____", count, "\n\n");
+		//text = text + "mush";
+		
+    		console.log(videoTitle + " " + count);
+})
+	console.log(text);	
+	tweetUrl = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(text) + "via @Zi88";
+	//console.log(postData);
+	window.open(tweetUrl);
+}
+
